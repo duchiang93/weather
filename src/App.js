d0fd26ec7c56912977377331e31ddbdde1207cb6
duchiang93 <103938574+duchiang93@users.noreply.github.com>
2022-07-24 22:41:19 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "@emotion/styled";
+import Icon from "./Icon";
+
+const Container = styled.div`
+  background-color: #ededed;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const WeatherCard = styled.div`
+  position: relative;
+  min-width: 360px;
+  box-shadow: 0 1px 3px 0 #999999;
+  background-color: #f9f9f9;
+  box-sizing: border-box;
+  padding: 30px 15px;
+`;
+
+const Location = styled.div`
+  font-size: 28px;
+  color: #212121;
+  margin-bottom: 20px;
+`;
+
+const Description = styled.div`
+  font-size: 16px;
+  color: #828282;
+  margin-bottom: 30px;
+`;
+
+const CurrentWeather = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const Temperature = styled.div`
+  color: #757575;
+  font-size: 96px;
+  font-weight: 300;
+  display: flex;
+`;
+
+const Celsius = styled.div`
+  font-weight: normal;
+  font-size: 42px;
+`;
+
+const AirFlow = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+  margin-bottom: 20px;
+`;
+
+const Rain = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: #828282;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <WeatherCard>
+        <Location>台北市</Location>
+        <Description>多雲時晴</Description>
+        <CurrentWeather>
+          <Temperature>
+            <Icon.CloudyIcon />
+            23<Celsius>°C</Celsius>
+          </Temperature>
+        </CurrentWeather>
+        <AirFlow>
+          <Icon.AirFlowIcon />
+          23 m/h
+        </AirFlow>
+        <Rain>
+          <Icon.RainIcon />
+          48%
+        </Rain>
+      </WeatherCard>
+    </Container>
   );
 }
 
---
+++ src/Icon.js
@@ -0,0 +1,9 @@
+import { ReactComponent as CloudyIcon } from "./IMG/cloudy.svg";
+import { ReactComponent as AirFlowIcon } from "./IMG/airFlow.svg";
+import { ReactComponent as RainIcon } from "./IMG/rain.svg";
+
+export default {
+  CloudyIcon,
+  AirFlowIcon,
+  RainIcon,
+};
